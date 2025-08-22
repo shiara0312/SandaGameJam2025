cecca17bd42decd198c794b10b6453f94f507dbe
Melissa <melissahuerta.dev@gmail.com>
2025-08-21 20:14:34 -0500

    feat: gestionar cola de clientes

--- sandagamejam/scripts/cliente.gd
+++ sandagamejam/scripts/cliente.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+var character_id: String
+var mood_id: String
+var texts: Dictionary
+
+func setup(data: Dictionary, language: String):
+	character_id = data["character_id"]
+	mood_id = data["mood_id"]
+	texts = data["texts"]
+	$Problematica.text = texts[language]
--- sandagamejam/scripts/global_manager.gd
+++ sandagamejam/scripts/global_manager.gd
@@ -2,8 +2,24 @@ extends Node
 
 var is_game_started : bool = false
 var game_language : String = "es" # pueden ser "en", "fr".
-
+var clientes_por_atender: Array = []
+var clientes_satisfechos: Array = []
 
 func start_game():
 	is_game_started = true
- 
+
+# Clonar los clientes obtenidos para el nivel 
+func inicializar_clientes(combos: Array):
+	clientes_por_atender = combos.duplicate()
+
+func obtener_siguiente_cliente() -> Dictionary:
+	if clientes_por_atender.is_empty():
+		return {}
+	# retorna el primer elemento del array
+	return clientes_por_atender.pop_front()
+
+func devolver_cliente(cliente: Dictionary):
+	clientes_por_atender.append(cliente)
+
+func marcar_como_satisfecho(cliente: Dictionary):
+	clientes_satisfechos.append(cliente)
