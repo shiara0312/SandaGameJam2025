4f711aa4692895ceeee2fe7747d77773aed8b43f
Melissa <melissahuerta.dev@gmail.com>
2025-08-24 18:17:47 -0500

    feat: recetas muestran ingredientes y botones de interaccion segun language

--- sandagamejam/scripts/levels/minigame_overlay.gd
+++ sandagamejam/scripts/levels/minigame_overlay.gd
@@ -23,15 +23,18 @@ func show_recipe_container():
 	recipe_container.visible = true
 
 func show_selected_recipe(idx: int) -> void:
+	GlobalManager.selected_recipe_idx = idx
 	load_selected_recipe_data(idx)
+	load_ingredients_assets()
 	hide_menu_container()
 	show_recipe_container()
 
 func load_selected_recipe_data(idx: int) -> void:
 	var lang = GlobalManager.game_language
+	print("current_level_recipes ", GlobalManager.current_level_recipes)
 	var recipe_selected = GlobalManager.current_level_recipes[idx]
 	var rec_name = recipe_selected["name"][lang]
-	var benefits = recipe_selected["benefits"][lang]
+	#var benefits = recipe_selected["benefits"][lang]
 	var riddle = recipe_selected["riddle"][lang]
 	
 	print("recipe_selected ", recipe_selected["ingredients"])
@@ -43,6 +46,46 @@ func load_selected_recipe_data(idx: int) -> void:
 	rich_label_text.bbcode_enabled = true
 	rich_label_text.text = text
 	
+func load_ingredients_assets():
+	var recipe_selected = GlobalManager.current_level_recipes[GlobalManager.selected_recipe_idx]
+	var ingredients = recipe_selected["ingredients"]
+
+	# Contenedor donde irán los ingredientes
+	var ing_container = recipe_container.get_node("IngredientsContainer")
+	clear_children(ing_container)
+
+
+	for i in range(ingredients.size()):
+		var ing_id = ingredients[i]
+		var path = "res://assets/pastry/ingredients/%s.png" % ing_id
+		if not ResourceLoader.exists(path):
+			print("⚠️ No existe asset:", path)
+			continue
+
+		var tex = load(path)
+		
+		# Wrapper (para escalarlo)
+		var wrapper = Control.new()
+		wrapper.custom_minimum_size = tex.get_size() * 0.25
+		wrapper.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+		wrapper.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+
+		var sprite = TextureRect.new()
+		sprite.texture = tex
+		sprite.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		sprite.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+		sprite.anchor_right = 1
+		sprite.anchor_bottom = 1
+		sprite.size_flags_horizontal = Control.SIZE_FILL
+		sprite.size_flags_vertical = Control.SIZE_FILL
+
+		wrapper.add_child(sprite)
+		ing_container.add_child(wrapper)
+
+func clear_children(node: Node) -> void:
+	for child in node.get_children():
+		child.queue_free()
+
 func _on_recipe_1_pressed() -> void:
 	show_selected_recipe(0)
 
