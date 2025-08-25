ea583508f86f48771e01caaccc64616d0e19600d
Melissa <melissahuerta.dev@gmail.com>
2025-08-25 17:32:37 -0500

    fix: position de cliente corregida

--- sandagamejam/scripts/customer/customer.gd
+++ sandagamejam/scripts/customer/customer.gd
@@ -6,14 +6,18 @@ signal listen_customer_pressed
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var btn_listen : TextureButton = $BtnListen
 @onready var sfx_entering : AudioStreamPlayer = $SFXEntering
-@onready var sfx_click : AudioStreamPlayer = $SFXClick
 @export var speed: float = 300.0 #DEBUG: 500.0
 
-var start_x = -200
-var offset_x = 100
+const BASE_VIEWPORT = Vector2(1152, 648)
+const BASE_START_X = -200
+const BASE_OFFSET_Y = 80
+const BASE_OFFSET_X = 100
+const SPRITE_POSITION_Y = 414
+const SPRITE_POSITION_X = 1152/2 + -80
+
 var target_y_ratio := 0.05
 var relative_x: float = 0.5
-var customer_scale: float = 0.165
+var customer_scale: float = 0.165 #escala de referencia
 var character_id: String
 var mood_id: String
 var texts: Dictionary
@@ -23,9 +27,21 @@ var language: String
 enum State { ENTERING, SEATED, SATISFIED }
 var state = State.ENTERING
 	
+func _ready():
+	pass
+	
+# Setup: Preparar, reinicializar data del cliente
+func setup(data: Dictionary, lang: String):
+	await ready
+	character_id = data["character_id"]
+	mood_id = data["mood_id"]
+	texts = data["texts"]
+	language = lang
+	# Buscar el botón de forma segura (sin depender de @onready aún)
+	btn_listen.visible = false
+
 # Desde CafeLevel1 se llama a:
 func move_to(target_position: Vector2) -> void:
-	#print("DEBUG > move_to: Customer started moving to...")
 	sfx_entering.play()
 	
 	var dist := (target_position - position).length()
@@ -36,9 +52,8 @@ func move_to(target_position: Vector2) -> void:
 	tween.finished.connect(customer_positioned)
 
 func customer_positioned():
-	print("DEBUG > customer_positioned: Cliente llegó al centro")
 	sfx_entering.stop()
-	set_state(State.SEATED) #TODO: Cambiar sprite/animación a sentado
+	set_state(State.SEATED)
 	
 	var label = btn_listen.get_node("Label")
 	label.text = GlobalManager.btn_listen_customer_label
@@ -46,22 +61,7 @@ func customer_positioned():
 	
 	emit_signal("arrived_at_center", self)
 
-# Setup: Preparar, reinicializar data del cliente
-func setup(data: Dictionary, lang: String):
-	await ready
-	character_id = data["character_id"]
-	mood_id = data["mood_id"]
-	texts = data["texts"]
-	language = lang
-	# Buscar el botón de forma segura (sin depender de @onready aún)
-	btn_listen.visible = false
-	# Escala para el sprite
-	sprite.scale = Vector2(customer_scale, customer_scale)
-
-
 func set_state(new_state: State):
-	print(">> SETTING STATE ", new_state)
-	print("my customer ", character_id, " ", mood_id)
 	state = new_state
 	match state:
 		State.ENTERING:
@@ -83,7 +83,7 @@ func set_state(new_state: State):
 # Colocar el botón justo arriba del sprite
 func position_listen_button():
 	if sprite and sprite.texture:
-		var texture_size = sprite.texture.get_size() * sprite.scale
+		var texture_size = get_sprite_size()
 		var btn_size = btn_listen.size
 		
 		# centrar en X, arriba en Y
@@ -93,26 +93,17 @@ func position_listen_button():
 		btn_listen.position = Vector2(x, y)
 
 #Cargar sprite y aplicar escala.
-func get_scaled_size() -> Vector2:
+func get_sprite_size() -> Vector2:
 	if sprite.texture:
 		return sprite.texture.get_size() * sprite.scale
 	return Vector2.ZERO
-	
-#Calcular su posición inicial (fuera de pantalla, con margen inferior).
+
 func get_initial_position(viewport_size: Vector2) -> Vector2:
-	var sprite_size = get_scaled_size()
-	var margin_bottom = viewport_size.y * target_y_ratio
-	var y = viewport_size.y - (sprite_size.y / 2 + margin_bottom)
-	return Vector2(start_x, y)
+	return Vector2(BASE_START_X, SPRITE_POSITION_Y)
 
-#Calcular su posición final (centro X, con margen inferior).
 func get_target_position(viewport_size: Vector2) -> Vector2:
-	var sprite_size = get_scaled_size()
-	var margin_bottom = viewport_size.y * target_y_ratio
-
-	var y = viewport_size.y - (sprite_size.y / 2 + margin_bottom)
-	return Vector2(viewport_size.x / 2 - offset_x, y)
-
+	return Vector2(SPRITE_POSITION_X, SPRITE_POSITION_Y)
+	
 func _on_btn_listen_pressed() -> void:
 	AudioManager.play_click_sfx()
 	emit_signal("listen_customer_pressed")
@@ -127,3 +118,10 @@ func load_customer_texture(path: String, alt_path: String):
 		tex = load(alt_path)
 		
 	sprite.texture = tex
+	
+# Obtener factor uniforme para la escala
+func get_scale_factor():
+	var viewport_size: Vector2 = get_viewport().size
+	var scale_factor = min(viewport_size.x / BASE_VIEWPORT.x, viewport_size.y / BASE_VIEWPORT.y)
+	
+	return scale_factor
