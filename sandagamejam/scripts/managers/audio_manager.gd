a842196dd3c5005313a8a23c1f6e0beef2eedc63
Melissa <melissahuerta.dev@gmail.com>
2025-08-26 03:06:51 -0500

    feat: audios agregados para efecto batidora, recolectar ingre, buena y mala receta

--- sandagamejam/scripts/managers/audio_manager.gd
+++ sandagamejam/scripts/managers/audio_manager.gd
@@ -1,6 +1,10 @@
 extends Node
 
 @onready var sfx_click: AudioStreamPlayer = $SFXClick
+@onready var sfx_collect_ingredient: AudioStreamPlayer = $SFXCollectIngredient
+@onready var sfx_right_recipe: AudioStreamPlayer = $SFXRightRecipe
+@onready var sfx_wrong_recipe: AudioStreamPlayer = $SFXWrongRecipe
+@onready var sfx_whisking: AudioStreamPlayer = $SFXWhisking
 @onready var game_music: AudioStreamPlayer = $GameMusic
 
 func play_click_sfx():	
@@ -9,6 +13,31 @@ func play_click_sfx():
 	else:
 		push_warning("SFXClick no está asignado o no existe en AudioManager")
 		
+func play_collect_ingredient_sfx():	
+	if sfx_collect_ingredient:
+		sfx_collect_ingredient.play()
+	else:
+		push_warning("SFXCollectIngredient no está asignado o no existe en AudioManager")
+		
+func play_right_recipe_sfx():	
+	if sfx_right_recipe:
+		sfx_right_recipe.play()
+	else:
+		push_warning("SFXRightRecipe no está asignado o no existe en AudioManager")
+		
+func play_wrong_recipe_sfx():	
+	if sfx_wrong_recipe:
+		sfx_wrong_recipe.play()
+	else:
+		push_warning("SFXCollectIngredient no está asignado o no existe en AudioManager")
+
+func play_whisking_sfx():	
+	if sfx_whisking:
+		sfx_whisking.play()
+	else:
+		push_warning("SFXWhisking no está asignado o no existe en AudioManager")
+		
+
 func play_game_music():	
 	if game_music:
 		game_music.play()
