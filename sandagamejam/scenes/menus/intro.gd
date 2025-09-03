53cd7679a6e3927551ecd6b1092fc78941e1dd02
shiara <shiara.arauzo@gmail.com>
2025-09-03 00:46:00 -0500

    animacion de intro

--- sandagamejam/scenes/menus/intro.gd
+++ sandagamejam/scenes/menus/intro.gd
@@ -10,22 +10,56 @@ var texts = [
 	"Ahora debe combinar ingredientes secretos para complacer a sus clientes exigentes...",
 	"¡Tu aventura como maestro pastelero comienza ahora!"
 ]
+var typing_speed := 0.05
+var typing_timer: Timer
+var full_text := ""
+var char_index := 0
 var current_index = 0
+var is_typing := false
+
 func _ready():
-	text_intro.text = texts[current_index]
+	# animacion
+	typing_timer = Timer.new()
+	typing_timer.wait_time = typing_speed
+	typing_timer.one_shot = false
+	typing_timer.timeout.connect(Callable(self, "_on_typing_timeout"))
+	add_child(typing_timer)
+	
+	_show_text(texts[current_index])
+	
 	button_label.text = "Continuar" 
 	btn_start.pressed.connect(_on_btn_start_pressed)
 	if has_node("BtnBack"):
 		btn_back.pressed.connect(_on_btn_back_pressed)
 
+func _show_text(new_text: String):
+	full_text = new_text
+	char_index = 0
+	text_intro.text = ""
+	is_typing = true
+	typing_timer.start()
+	
+func _on_typing_timeout():
+	if char_index < full_text.length():
+		text_intro.text += full_text[char_index]
+		char_index += 1
+	else:
+		typing_timer.stop()
+		is_typing = false
+
 func _on_btn_start_pressed():
 	
 	AudioManager.play_click_sfx()
+	
+	if is_typing:
+		text_intro.text = full_text
+		typing_timer.stop()
+		is_typing = false
+		return
+		
 	current_index += 1
 	if current_index < texts.size():
-
-		text_intro.text = texts[current_index]
-
+		_show_text(texts[current_index])
 
 		if current_index == texts.size() - 1:
 			button_label.text = "Jugar"
