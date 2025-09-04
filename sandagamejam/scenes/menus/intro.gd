c330252177515b72c1edaa7676ba15ce36861812
shiara <shiara.arauzo@gmail.com>
2025-09-03 20:36:35 -0500

    nuevo fondo de intro

--- sandagamejam/scenes/menus/intro.gd
+++ sandagamejam/scenes/menus/intro.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
 @onready var btn_start: TextureButton = $Comenzar_Game
-@onready var button_label: Label = $Comenzar_Game/button_label
-@onready var text_intro = $text_intro
-@onready var btn_back: Button = $BtnBack if has_node("BtnBack") else null # si tienes un botón volver
+@onready var text_intro: Label = $TextureRect/text_intro
+@onready var btn_back: Button = $BtnBack if has_node("BtnBack") else null 
 
 var texts = [
 	"Sir Isaac Newton, cansado de que le caigan manzanas en la cabeza, abrió una pastelería...",
@@ -18,7 +17,7 @@ var current_index = 0
 var is_typing := false
 
 func _ready():
-	# animacion
+	# animación de tipeo
 	typing_timer = Timer.new()
 	typing_timer.wait_time = typing_speed
 	typing_timer.one_shot = false
@@ -27,7 +26,6 @@ func _ready():
 	
 	_show_text(texts[current_index])
 	
-	button_label.text = "Continuar" 
 	btn_start.pressed.connect(_on_btn_start_pressed)
 	if has_node("BtnBack"):
 		btn_back.pressed.connect(_on_btn_back_pressed)
@@ -48,7 +46,6 @@ func _on_typing_timeout():
 		is_typing = false
 
 func _on_btn_start_pressed():
-	
 	AudioManager.play_click_sfx()
 	
 	if is_typing:
@@ -60,16 +57,12 @@ func _on_btn_start_pressed():
 	current_index += 1
 	if current_index < texts.size():
 		_show_text(texts[current_index])
-
-		if current_index == texts.size() - 1:
-			button_label.text = "Jugar"
 	else:
-		# Tercer click iniciar juego
+		# Tercer clic → iniciar juego
 		var level_1_path = "res://scenes/levels/PastryLevel1.tscn"
 		GlobalManager.start_game()
 		GameController.load_level(level_1_path)
 		GameController.show_newton_layer()
-
 
 func _on_btn_back_pressed():
 	AudioManager.play_click_sfx()
